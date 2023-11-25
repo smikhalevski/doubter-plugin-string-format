145f11f43b30c9ddcadbd51b6947b5e6fb673bf5
smikhalevski <smikhalevski@gmail.com>
2023-11-25 17:53:35 +0300

    Fixed validator imports

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,19 @@ const typescript = require('@rollup/plugin-typescript');
 module.exports = {
   input: './src/main/index.ts',
   output: [
-    { format: 'cjs', entryFileNames: '[name].js', dir: './lib', preserveModules: true },
-    { format: 'es', entryFileNames: '[name].mjs', dir: './lib', preserveModules: true },
+    {
+      format: 'cjs',
+      entryFileNames: '[name].js',
+      dir: './lib',
+      preserveModules: true,
+    },
+    {
+      format: 'es',
+      entryFileNames: '[name].mjs',
+      dir: './lib',
+      preserveModules: true,
+      paths: id => id.startsWith('validator') && id.replace('validator', 'validator/es'),
+    },
   ],
   plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
   external: /^doubter|^validator/,
